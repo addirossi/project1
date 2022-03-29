990252491e20c9540fdb2fc5ef0c4e94b64de099
addirossi <addirossi@gmail.com>
2022-03-29 11:23:51 +0600

    Add model2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import email
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,7 @@ from django.db import models
 class MyModel(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=150)
+
+class Model2(models.Model):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
