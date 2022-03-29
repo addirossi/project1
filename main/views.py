d9968941f7fe7d10c17f32e5ab58bde2d96dc35e
timur <timur.dootaliev.td@gmail.com>
2022-03-29 11:44:27 +0600

    add user list

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import MyModel
-
+from django.contrib.auth import get_user_model
 
 def index_page(request):
     return render(request, 'index.html', {})
@@ -9,3 +9,8 @@ def index_page(request):
 def products_list(request):
     products = MyModel.objects.all()
     return render(request, 'main/products_list.html', {'products': products})
+
+
+def user_list(request):
+    users = get_user_model().objects.all()
+    return render(request, 'main/users.html', {'users': users})
